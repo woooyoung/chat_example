2a3fd6f82478d5e79f6422f696a7c4091471eb87
woooyoung <axdsw121@gmail.com>
2025-02-03 16:23:08 +0900

    json 형태로 데이터 전달, record

--- src/main/java/com/koreait/exam/chat_example/ChatController.java
+++ src/main/java/com/koreait/exam/chat_example/ChatController.java
@@ -1,9 +1,7 @@
 package com.koreait.exam.chat_example;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,19 +12,44 @@ public class ChatController {
 
     private List<ChatMessage> chatMessages = new ArrayList<ChatMessage>();
 
+//    @AllArgsConstructor
+//    @Getter
+//    public static class writeChatMessageRequest {
+//        private final String authorName;
+//        private final String content;
+//    }
+
+    public record writeChatMessageRequest(String authorName, String content) {
+    }
+
     public record writeChatMessageResponse(long id) {
 
     }
 
     @PostMapping("/writeMessage")
     @ResponseBody
-    public RsData<writeChatMessageResponse> writeMessage() {
-        ChatMessage message = new ChatMessage("김철수", "안녕");
+    public RsData<writeChatMessageResponse> writeMessage(@RequestBody writeChatMessageRequest req) {
+        ChatMessage message = new ChatMessage(req.authorName(), req.content());
         chatMessages.add(message);
         return new RsData<>(
                 "S-1",
                 "메세지가 작성됨",
                 new writeChatMessageResponse(message.getId())
+        );
+    }
+
+
+    public record messagesResponse(List<ChatMessage> messages, long count) {
+
+    }
+
+    @GetMapping("/messages")
+    @ResponseBody
+    public RsData<messagesResponse> messages() {
+        return new RsData<>(
+                "S-1",
+                "메세지 리스트",
+                new messagesResponse(chatMessages, chatMessages.size())
         );
     }
 
--- src/main/java/com/koreait/exam/chat_example/ChatMessage.java
+++ src/main/java/com/koreait/exam/chat_example/ChatMessage.java
@@ -1,5 +1,6 @@
 package com.koreait.exam.chat_example;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -10,6 +11,7 @@ import java.time.LocalDateTime;
 @Data
 public class ChatMessage {
     private long id;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss",timezone = "Asia/Seoul")
     private LocalDateTime createDate;
     private String authorName;
     private String content;
