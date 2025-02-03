b18963650a15957facd09c78b361f5e43a0928e8
woooyoung <axdsw121@gmail.com>
2025-02-03 15:46:36 +0900

    메세지 저장

--- src/main/java/com/koreait/exam/chat_example/ChatController.java
+++ src/main/java/com/koreait/exam/chat_example/ChatController.java
@@ -5,16 +5,29 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 @RequestMapping("/chat")
 public class ChatController {
 
-    ChatMessage message = new ChatMessage("김철수", "안녕");
+    private List<ChatMessage> chatMessages = new ArrayList<ChatMessage>();
+
+    public record writeChatMessageResponse(long id) {
+
+    }
 
     @PostMapping("/writeMessage")
     @ResponseBody
-    public RsData<ChatMessage> writeMessage() {
-        return new RsData<>("S-1", "메세지가 작성됨", message);
+    public RsData<writeChatMessageResponse> writeMessage() {
+        ChatMessage message = new ChatMessage("김철수", "안녕");
+        chatMessages.add(message);
+        return new RsData<>(
+                "S-1",
+                "메세지가 작성됨",
+                new writeChatMessageResponse(message.getId())
+        );
     }
 
 }
