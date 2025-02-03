49376bff9f909deca87fd4f58d30a0b5da2c2a2a
woooyoung <axdsw121@gmail.com>
2025-02-03 14:37:06 +0900

    프로젝트 세팅

---
+++ src/test/java/com/koreait/exam/chat_example/ChatExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koreait.exam.chat_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
