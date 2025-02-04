6cedb232a51aa13a0c6807ea09a900313518c3d5
woooyoung <axdsw121@gmail.com>
2025-02-04 10:31:01 +0900

    stream, filter

--- src/main/java/com/koreait/exam/chat_example/ChatController.java
+++ src/main/java/com/koreait/exam/chat_example/ChatController.java
@@ -1,23 +1,19 @@
 package com.koreait.exam.chat_example;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
 
 @Controller
 @RequestMapping("/chat")
+@Slf4j
 public class ChatController {
 
     private List<ChatMessage> chatMessages = new ArrayList<ChatMessage>();
-
-//    @AllArgsConstructor
-//    @Getter
-//    public static class writeChatMessageRequest {
-//        private final String authorName;
-//        private final String content;
-//    }
 
     public record writeChatMessageRequest(String authorName, String content) {
     }
@@ -38,6 +34,9 @@ public class ChatController {
         );
     }
 
+    public record messagesRequest(Long fromId) {
+
+    }
 
     public record messagesResponse(List<ChatMessage> messages, long count) {
 
@@ -45,12 +44,29 @@ public class ChatController {
 
     @GetMapping("/messages")
     @ResponseBody
-    public RsData<messagesResponse> messages() {
+    public RsData<messagesResponse> messages(messagesRequest req) {
+
+        List<ChatMessage> messages = chatMessages;
+
+        log.debug("req : {}", req);
+
+        // 번호가 같이 입력되었다면?
+        if (req.fromId != null) {
+            // 해당 번호의 채팅 메세지가 전체 리스트의 몇번째 인덱스인지? 없다면 -1
+            int index = IntStream.range(0, messages.size())
+                    .filter(i -> chatMessages.get(i).getId() == req.fromId)
+                    .findFirst().orElse(-1);
+
+            if (index != -1) {
+                // 만약에 인덱스가 -1이 아니라면? 0번부터 index번 까지 제거한 리스트를 만든다.
+                messages = messages.subList(index + 1, messages.size());
+            }
+        }
+
         return new RsData<>(
                 "S-1",
                 "메세지 리스트",
                 new messagesResponse(chatMessages, chatMessages.size())
         );
     }
-
 }
